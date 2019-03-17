1b3f4f35e2611597319670bab68b4e3c9d0953b8
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-17 23:17:02 +0000

    added login page

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ app.post('/api/posts', (req, res) => {
 	});
 });
 
-
+app.post('/api/login', (req, res) => {
+	// Mock User
+	const user = {
+		id: 1,
+		username: 'brad',
+		email: 'brad@gmail.com'
+	};
+	
+	jwt.sign({user}, 'secretkey', (err, token) => {
+		res.json({
+			token
+		});
+	});
+});
 
 app.listen(8080, () => console.log('Server started on port 8080'));
